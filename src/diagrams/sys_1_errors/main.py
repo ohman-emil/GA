2cb2e6788589d571f67d42e923263b69fd134a07
Emil Öhman <kameragubben04@outlook.com>
2022-11-17 19:02:09 +0100

    Changes to sys_1_errors diagram

--- src/diagrams/sys_1_errors/main.py
+++ src/diagrams/sys_1_errors/main.py
@@ -1,33 +1,44 @@
 import matplotlib.pyplot as plt
-from matplotlib import ticker
-from numpy import geomspace#, linspace
+from matplotlib import ticker, font_manager
+from numpy import geomspace
 import csv
 
+font_path = '/Users/emil.ohman/Library/Fonts/cmunrm.ttf' # Font path
+font_manager.fontManager.addfont(font_path) # Add font to matplotlib lib
+prop = font_manager.FontProperties(fname=font_path) # Get its properties
+
 data = []
-with open('src/systems/system1/errors.csv', 'r') as file:
+with open('src/systems/system1/errors.csv', 'r') as file: # Open data file
     reader = csv.reader(file)
-    [data.append(row) for row in reader]
+    [data.append(row) for row in reader] # Add each row to the data list
 
 plot_data = []
 for idx in range(len(data[0])):
-    plot_data.append([float(element[idx]) for element in data[1:]])
+    plot_data.append([float(element[idx]) for element in data[1:]]) # Convert data to float and change the data structure a bit, so every value in a column is grouped instead
 
+# matplotlib parameters
+params = {
+    "figure.figsize": (8.8, 4.4),
+    "font.family": "serif",
+    "font.serif": prop.get_name() # Get name from font_manager
+}
 
-plt.style.use('seaborn-v0_8-whitegrid')
-plt.rcParams["figure.figsize"] = (16,8)
-fig, ax = plt.subplots()
+plt.rcParams.update(params) # Add the params to matplotlib
+fig, ax = plt.subplots() # Create a subplot
 
+# Plot each equation one at a time
 plt.plot(plot_data[0], plot_data[1], marker='o', label='Euler Ekv. 1')
 plt.plot(plot_data[0], plot_data[2], marker='o', label='Euler Ekv. 2')
 plt.plot(plot_data[0], plot_data[3], marker='o', label='Heun Ekv. 1')
 plt.plot(plot_data[0], plot_data[4], marker='o', label='Heun Ekv. 2')
 
-ax.set_xscale('log')
+ax.set_xscale('log') # Make x-axis log scale
 ax.set_xlabel('Steglängd', fontsize='x-large')
 ax.set_ylabel('Avvikelse', fontsize='x-large')
-ax.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
-ax.set_xticks(geomspace(max(plot_data[0]), min(plot_data[0]), 5))
-ax.legend(loc='upper left', fontsize='x-large', frameon=True)
+ax.get_xaxis().set_major_formatter(ticker.ScalarFormatter()) # Add ticks to x-axis
+ax.set_xticks(geomspace(max(plot_data[0]), min(plot_data[0]), 5)) # Add 5 equally spaces ticks to x-axis
+ax.legend(loc='upper left', fontsize='x-large', frameon=True) # Add legend
+plt.grid() # Add grid lines
 
-plt.savefig('sys_1_errors.svg', bbox_inches='tight')
-plt.show()
+plt.savefig('sys_1_errors.svg', bbox_inches='tight') # Save figure
+plt.show() # And show it!
